52d5b632619a19710c803835a752324331b1e385
ChibaTomoki <michiamo.t1@gmail.com>
2023-05-05 01:44:55 +0900

    prettierの設定追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:@typescript-eslint/recommended', 'prettier'],
   overrides: [],
-  parser: "vue-eslint/parser",
+  parser: 'vue-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  plugins: ["vue", "@typescript-eslint"],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {},
-};
+}
