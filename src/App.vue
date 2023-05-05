65b26fe1706fe49ab7490ba878cd29859a79d163
ChibaTomoki <michiamo.t1@gmail.com>
2023-05-05 11:55:56 +0900

    一旦BEへのPOST成功を確認

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import axios from 'axios'
 import MyCanvas from './components/MyCanvas.vue'
 
 const base64CanvasImg = ref<string | null>(null)
+const postBase64CanvasImg = async (base64Img: string | null) => {
+  if (!base64Img) return
+
+  const res = await axios.post('http://localhost:8000/images/', {
+    image_base64: base64Img,
+  })
+  return res
+}
 </script>
 
 <template>
@@ -13,6 +22,7 @@ const base64CanvasImg = ref<string | null>(null)
       }
     "
   />
+  <button @click="postBase64CanvasImg(base64CanvasImg)">POST</button>
 </template>
 
 <style scoped></style>
