05b337a62fffd138727273e99dbf9f3c1337757e
ChibaTomoki <michiamo.t1@gmail.com>
2023-05-08 18:55:59 +0900

    BEのURLを環境変数化

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ const base64CanvasImg = ref<string | null>(null)
 const postBase64CanvasImg = async (base64Img: string | null) => {
   if (!base64Img) return
 
-  const res = await axios.post('http://localhost:8000/analyze-images/', {
+  const res = await axios.post(`${import.meta.env.VITE_BE_BASE_URL}/analyze-images/`, {
     image_base64: base64Img,
   })
   alert(`結果: ${res.data.num}\n確率: ${Math.round(res.data.prob * 100)}%`)
